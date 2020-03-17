15bec838c18fb231d177ca336cfd0406672b400d
EugeneFox27 <Eugenefox27@gmail.com>
2020-03-17 17:10:25 +0100

    add 2 files to the repoz

---
+++ script_udemy.js
@@ -0,0 +1,21 @@
+var money,
+timeData ;
+var now = new Date();
+var savings;
+var earnings;
+
+income = [savings = false, earnings = true];
+ expenses : {
+    money = prompt('Your budget for a month?', 1000);
+    timeData  = prompt('Insert a date in YYYY-MM-DD','2020-03-17');
+  }
+ optionalExpenses: {
+    this.money = prompt('Required expenses in a month?', 0);
+    this.count = prompt('How much it will cost?', 0);
+}
+
+
+
+
+
+
